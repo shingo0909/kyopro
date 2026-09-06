1c294115aa3307b940dc797fb16d10a291d47334
mabo8128 <mabo8128@gmail.com>
2026-09-06 17:48:12 +0900

    Update library files

--- Library/PrimeTable.cpp
+++ Library/PrimeTable.cpp
@@ -9,26 +9,43 @@ struct PrimeTable {
     int n;
     vector<int> spf;
     vector<int> primes;
+    vector<int> phi_table;
+    vector<int> mu_table;
+
     void build() {
         spf[0] = -1;
         spf[1] = -1;
+        phi_table[1] = 1;
+        mu_table[1] = 1;
 
         for (int i = 2; i <= n; i++) {
             if (spf[i] == 0) {
                 spf[i] = i;
                 primes.push_back(i);
+                phi_table[i] = i - 1;
+                mu_table[i] = -1;
             }
             for (int p : primes) {
                 if (p > spf[i] || 1LL * p * i > n)
                     break;
                 spf[p * i] = p;
+                if (p == spf[i]) {
+                    phi_table[p * i] = phi_table[i] * p;
+                    mu_table[p * i] = 0;
+                } else {
+                    phi_table[p * i] = phi_table[i] * (p - 1);
+                    mu_table[p * i] = -mu_table[i];
+                }
             }
         }
         return;
     }
 
   public:
-    PrimeTable(int n) : n(n), spf(n + 1, 0) {
+    PrimeTable(int n) : n(n),
+                        spf(n + 1, 0),
+                        phi_table(n + 1, 0),
+                        mu_table(n + 1, 0) {
         build();
     }
 
@@ -67,5 +84,14 @@ struct PrimeTable {
             }
         }
         return res;
+    }
+
+    // オイラーのトーシェント関数 φ(x)
+    long long phi(int x) const {
+        return phi_table[x];
+    }
+    // メビウス関数 μ(x)
+    int mu(int x) const {
+        return mu_table[x];
     }
 };
